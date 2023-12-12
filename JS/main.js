1ec5e51468e83efa7fadc0ee98b6ee89fd1bd72a
Jinwoooooooo <jinwooya55@naver.com>
2023-12-12 15:20:09 +0900

    12.12

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,3 @@
-let backicon = document.getElementsByClassName("backicon");
-let nextbtn = document.getElementsByClassName("nextbtn");
-
 $(document).ready(function () {
     // .poster 이미지 클릭 시
     $(".poster").click(function () {
@@ -32,6 +29,39 @@ $(document).ready(function () {
         $(".movieList").show();
     });
 
+    //! Back Button
+    //? Seat에서 backicon을 클릭하면 Ticket이 나옴
+    $(".Seat .backicon").click(function () {
+        $(".Seat").hide();
+        $(".Ticket").show();
+    });
+    //? Ticket에서 backicon을 클릭하면 movieList가 나오고
+    //? 인원수 선택을 초기화 한다.
+    $(".Ticket .backicon").click(function () {
+        $(".adult .number").not(".zero").css({
+            "background-color": "#eeeeee",
+            "color": "#000000"
+        });
+    
+        $(".teenager .number").not(".zero").css({
+            "background-color": "#eeeeee",
+            "color": "#000000"
+        });
+    
+        $(".zero").css({
+            "background-color": "rgb(250, 130, 115)",
+            "color": "white"
+        });
+    
+        $(".Ticket").hide();
+        $(".movieList").show();
+    });
+
+    //? seat를 클릭하면 nextbtn2가 나옴
+    $(".seat").click(function () {
+        $(".nextbtn2").css("visibility", "");
+    });
+
     //! Next Button
     //? Ticket에서 nextbtn을 클릭하면 seat이 나옴
     $(".Ticket .nextbtn").click(function () {
@@ -39,17 +69,53 @@ $(document).ready(function () {
         $(".Seat").show();
     });
 
-    //! Back Button
-    //? Seat에서 backicon을 클릭하면 Ticket이 나옴
-    $(".Seat .backicon").click(function () {
-        $(".Seat").hide();
-        $(".Ticket").show();
+    //! Number Button
+    let selectedAdultNumber = null; // 성인 관람 인원을 추적하기 위한 변수
+    let selectedTeenagerNumber = null; // 청소년 관람 인원을 추적하기 위한 변수
+
+    // 일반(adult) 버튼 클릭 시
+    $(".adult .number").click(function () {
+        // 다른 숫자 버튼의 스타일 초기화
+        $(".adult .number").not(this).css({
+            "background-color": "#eeeeee",
+            "color": "#777777"
+        });
+
+        // 현재 클릭된 숫자 버튼에 스타일 설정
+        $(this).css({
+            "background-color": "rgb(250, 130, 115)",
+            "color": "white"
+        });
+
+        // 현재 클릭된 숫자를 추적
+        selectedAdultNumber = $(this).text();
+        if(selectedAdultNumber > 0) {
+            $(".nextbtn").css("visibility", "visible");
+        } else if(selectedAdultNumber == 0) {
+            $(".nextbtn").css("visibility", "hidden");
+        }
     });
 
-    //? Ticket에서 backicon을 누르면 movieList가 나옴
-    $(".Ticket .backicon").click(function () {
-        $(".Ticket").hide();
-        $(".movieList").show();
-    });
+    // 청소년(teenager) 버튼 클릭 시
+    $(".teenager .number").click(function () {
+        // 다른 숫자 버튼의 스타일 초기화
+        $(".teenager .number").not(this).css({
+            "background-color": "#eeeeee",
+            "color": "#777777"
+        });
 
+        // 현재 클릭된 숫자 버튼에 스타일 설정
+        $(this).css({
+            "background-color": "rgb(250, 130, 115)",
+            "color": "white"
+        });
+
+        // 현재 클릭된 숫자를 추적
+        selectedTeenagerNumber = $(this).text();
+        if(selectedTeenagerNumber > 0) {
+            $(".nextbtn").css("visibility", "visible");
+        }   else if(selectedTeenagerNumber == 0) {
+            $(".nextbtn").css("visibility", "hidden");
+        }
+    });
 });
